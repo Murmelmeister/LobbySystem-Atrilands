a18f0d50a5a84a0fbd563e6940fca1352171dd1e
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-28 22:08:07 +0200

    refactor ListenerDamage to improve constructor usage and simplify event handling

--- src/main/java/de/murmelmeister/lobbysystem/listeners/ListenerDamage.java
+++ src/main/java/de/murmelmeister/lobbysystem/listeners/ListenerDamage.java
@@ -1,21 +1,23 @@
 package de.murmelmeister.lobbysystem.listeners;
 
+import de.murmelmeister.lobbysystem.LobbySystem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class ListenerDamage extends Listeners {
-
-    @EventHandler(priority = EventPriority.HIGH)
-    public void handlePlayerFallDamage(EntityDamageEvent event) {
-        if(event.getEntity() instanceof Player) {
-            event.setCancelled(event.getCause().equals(EntityDamageEvent.DamageCause.FALL));
-        }
+    public ListenerDamage(LobbySystem plugin) {
+        super(plugin);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler
+    public void handlePlayerFallDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player)
+            event.setCancelled(event.getCause().equals(EntityDamageEvent.DamageCause.FALL));
+    }
+
+    @EventHandler
     public void handleEntityDamage(EntityDamageByEntityEvent event) {
         event.setCancelled(true);
     }
