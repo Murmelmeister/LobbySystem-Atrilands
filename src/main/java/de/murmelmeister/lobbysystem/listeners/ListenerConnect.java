ae9686d3a6a4a4c14a93bdd1df300aa6c75303b8
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-28 22:07:15 +0200

    refactor ListenerConnect to improve player join/quit handling and enhance message management

--- src/main/java/de/murmelmeister/lobbysystem/listeners/ListenerConnect.java
+++ src/main/java/de/murmelmeister/lobbysystem/listeners/ListenerConnect.java
@@ -1,50 +1,58 @@
 package de.murmelmeister.lobbysystem.listeners;
 
-import de.murmelmeister.lobbysystem.utils.scoreboards.TestScoreboard;
+import de.murmelmeister.lobbysystem.LobbySystem;
+import de.murmelmeister.lobbysystem.config.MessageConfig;
+import de.murmelmeister.lobbysystem.utils.LobbyItems;
+import de.murmelmeister.lobbysystem.utils.LocationUtil;
+import de.murmelmeister.lobbysystem.utils.Messages;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.util.Objects;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class ListenerConnect extends Listeners {
+    public ListenerConnect(LobbySystem plugin) {
+        super(plugin);
+    }
 
-    @EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler
     public void handlePlayerJoin(PlayerJoinEvent event) {
+        MessageConfig messageConfig = getMessageConfig();
+        LocationUtil locationUtil = getLocationUtil();
+        LobbyItems lobbyItems = getLobbyItems();
+        Map<UUID, Float> rainbowHue = getPlugin().getRainbowHue();
+
         event.joinMessage(null);
         Player player = event.getPlayer();
 
-        this.lobbyItems.setLobbyItems(player);
+        lobbyItems.setLobbyItems(player);
 
         // new TestScoreboard(player);
 
-        if (player.hasPermission(Objects.requireNonNull(this.messageConfig.getConfig().getString("Permission.LobbyItem.Boots")))) {
-            this.arrayListUtil.getHue().put(player.getUniqueId(), 0.0f);
-        }
+        if (player.hasPermission(messageConfig.getMessage(Messages.PERMISSION_LOBBY_ITEMS_BOOTS)))
+            rainbowHue.put(player.getUniqueId(), 0.0f);
 
         // When the spawn does not exist
-        try {
-            player.teleport(this.locationUtil.getLocation("Spawn"));
-        } catch (NullPointerException | IllegalArgumentException e) {
-            setSendMessage(player, "#777777--- #ff0000Setup #777777---");
-            setSendMessage(player, "#999999Setze bitte den Spawn. #777777(#ff0000/setspawn#777777)");
-        }
+        if (locationUtil.locationIsExisting("Spawn"))
+            player.teleport(locationUtil.getLocation("Spawn"));
+        else sendMessage(player, messageConfig.getMessage(Messages.MESSAGE_SPAWN_NOT_SET));
 
         // When the death height does not exist
-        if (!(this.locationUtil.heightIsExisting())) {
-            setSendMessage(player, "#777777--- #ff0000Setup #777777---");
-            setSendMessage(player, "#999999Setze bitte die todes Höhe. #777777(#ff0000/setdeathheight#777777)");
-        }
+        if (!locationUtil.heightIsExisting())
+            sendMessage(player, messageConfig.getMessage(Messages.MESSAGE_DEATH_HEIGHT_NOT_SET));
     }
 
-    @EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler
     public void handlePlayerQuit(PlayerQuitEvent event) {
+        List<UUID> buildMode = getPlugin().getBuildMode();
         Player player = event.getPlayer();
 
         // When the player disconnect
-        this.arrayListUtil.getBuildmode().remove(player.getUniqueId());
+        buildMode.remove(player.getUniqueId());
 
         event.quitMessage(null);
     }
