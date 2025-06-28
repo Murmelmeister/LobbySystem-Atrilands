f7e17afd3ccfab892e1e31963c6381f495bb50b1
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-28 22:22:37 +0200

    refactor MessageConfig to utilize FileUtil for file creation

--- src/main/java/de/murmelmeister/lobbysystem/config/MessageConfig.java
+++ src/main/java/de/murmelmeister/lobbysystem/config/MessageConfig.java
@@ -1,5 +1,6 @@
 package de.murmelmeister.lobbysystem.config;
 
+import de.murmelmeister.lobbysystem.utils.FileUtil;
 import de.murmelmeister.lobbysystem.utils.LobbyItems;
 import de.murmelmeister.lobbysystem.utils.Messages;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -23,21 +24,7 @@ public class MessageConfig {
     }
 
     private void create() {
-        this.file = new File("./plugins/" + LobbyItems.class.getSimpleName() + "/", "message.yml");
-        final File parent = file.getParentFile();
-        if (!parent.exists()) {
-            boolean success = parent.mkdirs();
-            if (!success) logger.warn("Could not create directory: {}", parent.getAbsolutePath());
-        }
-
-        if (!file.exists()) {
-            try {
-                boolean success = file.createNewFile();
-                if (!success) logger.warn("Could not create file: {}", file.getAbsolutePath());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        this.file = FileUtil.createFile(logger, "./plugins/" + LobbyItems.class.getSimpleName() + "/", "message.yml");
         this.config = YamlConfiguration.loadConfiguration(file);
     }
 
