a0fd9808a375c16338202b3de55f6dd23687cdce
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-28 23:07:48 +0200

    refactor Messages to improve permission handling and message clarity

--- src/main/java/de/murmelmeister/lobbysystem/utils/Messages.java
+++ src/main/java/de/murmelmeister/lobbysystem/utils/Messages.java
@@ -7,16 +7,13 @@ public enum Messages {
     PERMISSION_DEATH_HEIGHT_GET("Permission.DeathHeight.Get", "lobbysystem.command.deathheight.get"),
     PERMISSION_DEATH_HEIGHT_SET("Permission.DeathHeight.Set", "lobbysystem.command.deathheight.set"),
     PERMISSION_SPAWN_SET("Permission.Spawn.Set", "lobbysystem.command.spawn.set"),
-    PERMISSION_SPAWN_USE("Permission.Spawn.Use", "lobbysystem.command.spawn.use"),
-    PERMISSION_SPAWN_OTHER("Permission.Spawn.Other", "lobbysystem.command.spawn.other"),
-    PERMISSION_LOBBY_SYSTEM_USE("Permission.LobbySystem.Use", "lobbysystem.command.lobbysystem.use"),
-    PERMISSION_LOBBY_SYSTEM_RELOAD("Permission.LobbySystem.Reload", "lobbysystem.command.lobbysystem.reload"),
+    PERMISSION_SPAWN_GET("Permission.Spawn.Get", "lobbysystem.command.spawn.get"),
+    PERMISSION_LOBBY_SYSTEM_RELOAD("Permission.LobbySystemReload", "lobbysystem.command.reload"),
     PERMISSION_LOBBY_ITEMS_BOOTS("Permission.LobbyItems.Boots", "lobbysystem.lobbyitem.boots"),
     MESSAGE_NO_PERMISSION("Message.NoPermission", "<#ff0000>Du hast keine Berechtigung, diesen Befehl zu benutzen."),
     MESSAGE_NO_CONSOLE("Message.NoConsole", "<#ff0000>Dieser Befehl kann nicht von der Konsole ausgeführt werden."),
     MESSAGE_PLAYER_IS_NOT_ONLINE("Message.PlayerIsNotOnline", "<#ff0000>Der Spieler <#ffb500>[PLAYER]</#ffb500> ist nicht online."),
     MESSAGE_LOBBY_SYSTEM_RELOAD("Message.LobbySystem.ReloadMessage", "<#00ff00>Die Konfiguration wurde erfolgreich neu geladen."),
-    MESSAGE_LOBBY_SYSTEM_SYNTAX("Message.LobbySystem.Syntax", "<#ff0000>Syntax: /lobbysystem [reload]"),
     MESSAGE_BUILD_USE_DISABLED("Message.Build.UseDisabled", "<#999999>Das hast den Bau-Modus <#ff0000>deaktiviert</#ff0000>."),
     MESSAGE_BUILD_USE_ENABLED("Message.Build.UseEnabled", "<#999999>Du hast den Bau-Modus <#00ff00>aktiviert</#00ff00>."),
     MESSAGE_BUILD_OTHER_DISABLED("Message.Build.OtherDisabled", "<#999999>Du hast den Spieler <#ffb500>[PLAYER]</#ffb500> den Bau-Modus <#ff0000>deaktiviert</#ff0000>."),
@@ -27,8 +24,7 @@ public enum Messages {
     MESSAGE_DEATH_HEIGHT_NOT_SET("Message.DeathHeight.NotSet", "<#ff0000>Die Todeshöhe ist nicht gesetzt."),
     MESSAGE_SPAWN_SET("Message.Spawn.Set", "<#999999>Der Spawn wurde auf die aktuelle Position gesetzt."),
     MESSAGE_SPAWN_GET("Message.Spawn.Get", "<#999999>Du hast dich zum <#00ff00>Spawn</#00ff00> teleportiert."),
-    MESSAGE_SPAWN_NOT_SET("Message.Spawn.NotSet", "<#ff0000>Der Spawn ist nicht gesetzt."),
-    ;
+    MESSAGE_SPAWN_NOT_SET("Message.Spawn.NotSet", "<#ff0000>Der Spawn ist nicht gesetzt.");
     public static final Messages[] VALUES = values();
 
     private final String path;
