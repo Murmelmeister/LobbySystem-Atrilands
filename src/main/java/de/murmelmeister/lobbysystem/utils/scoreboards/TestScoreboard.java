ae885b661114efc9d610da9d34e68c7d3effa4bd
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-29 00:18:44 +0200

    refactor: integrate Economy instance into TestScoreboard for dynamic score updates

--- src/main/java/de/murmelmeister/lobbysystem/utils/scoreboards/TestScoreboard.java
+++ src/main/java/de/murmelmeister/lobbysystem/utils/scoreboards/TestScoreboard.java
@@ -1,9 +1,13 @@
 package de.murmelmeister.lobbysystem.utils.scoreboards;
 
+import de.murmelmeister.lobbysystem.LobbySystem;
+import de.murmelmeister.lobbysystem.api.Economy;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
 public class TestScoreboard extends ScoreboardBuilder {
+
+    private final Economy economy = LobbySystem.getInstance().getEconomy();
 
     public TestScoreboard(Player player, Component displayName) {
         super(player, displayName);
@@ -12,7 +16,7 @@ public class TestScoreboard extends ScoreboardBuilder {
 
     @Override
     public void createScoreboard() {
-
+        setScoreContent("" + economy.getMoney(player.getUniqueId()), 1);
     }
 
     @Override
