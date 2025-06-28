5deed5b774b04d857bcee7ff660c8ddf64ec6572
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-28 22:59:43 +0200

    refactor CommandSpawn to improve permission handling and message clarity

--- src/main/java/de/murmelmeister/lobbysystem/commands/CommandSpawn.java
+++ src/main/java/de/murmelmeister/lobbysystem/commands/CommandSpawn.java
@@ -1,64 +1,46 @@
 package de.murmelmeister.lobbysystem.commands;
 
-import de.murmelmeister.lobbysystem.utils.HexColor;
+import de.murmelmeister.lobbysystem.LobbySystem;
+import de.murmelmeister.lobbysystem.api.Locations;
+import de.murmelmeister.lobbysystem.config.MessageConfig;
+import de.murmelmeister.lobbysystem.utils.Messages;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
-public class CommandSpawn extends Commands {
+public final class CommandSpawn extends Commands {
+    public CommandSpawn(LobbySystem plugin) {
+        super(plugin);
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-
-        if (!(sender.hasPermission(Objects.requireNonNull(this.messageConfig.getConfig().getString("Permission.Spawn.Use"))))) {
-            setSendMessage(sender, HexColor.format(this.messageConfig.getConfig().getString("Message.NoPermission")));
+        MessageConfig messageConfig = getMessageConfig();
+        Locations locations = getLocations();
+        if (!(sender.hasPermission(messageConfig.getMessage(Messages.PERMISSION_SPAWN_GET)))) {
+            sendMessage(sender, messageConfig.getMessage(Messages.MESSAGE_NO_PERMISSION));
             return true;
         }
 
-        if (args.length == 0) {
+        Player player = sender instanceof Player ? (Player) sender : null;
 
-            Player player = sender instanceof Player ? (Player) sender : null;
-
-            if (player == null) {
-                setSendMessage(sender, HexColor.format(this.messageConfig.getConfig().getString("Message.NoConsole")));
-                return true;
-            }
-
-            player.teleport(this.locationUtil.getLocation("Spawn"));
-            setSendMessage(player, HexColor.format(this.messageConfig.getConfig().getString("Message.Spawn.Teleport")));
-
-        } else if (args.length == 1) {
-
-            if (!(sender.hasPermission(Objects.requireNonNull(this.messageConfig.getConfig().getString("Permission.Spawn.Other"))))) {
-                setSendMessage(sender, HexColor.format(this.messageConfig.getConfig().getString("Message.NoPermission")));
-                return true;
-            }
-
-            Player target = sender.getServer().getPlayer(args[0]);
-
-            if (target == null) {
-                setSendMessage(sender, HexColor.format(Objects.requireNonNull(this.messageConfig.getConfig().getString("Message.PlayerIsNotOnline")).replace("[PLAYER]", args[0])));
-                return true;
-            }
-
-            target.teleport(this.locationUtil.getLocation("Spawn"));
-            setSendMessage(target, HexColor.format(this.messageConfig.getConfig().getString("Message.Spawn.Teleport")));
-            setSendMessage(sender, HexColor.format(Objects.requireNonNull(this.messageConfig.getConfig().getString("Message.Spawn.TeleportOther")).replace("[PLAYER]", args[0])));
-
-        } else {
-            setSendMessage(sender, HexColor.format(this.messageConfig.getConfig().getString("Message.Spawn.Syntax")));
+        if (player == null) {
+            sendMessage(sender, messageConfig.getMessage(Messages.MESSAGE_NO_CONSOLE));
+            return true;
         }
 
+        player.teleport(locations.getLocation("Spawn"));
+        sendMessage(player, messageConfig.getMessage(Messages.MESSAGE_SPAWN_GET));
         return true;
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        return getTabComplete(sender, args);
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String @NotNull [] args) {
+        return Collections.emptyList();
     }
 
 }
