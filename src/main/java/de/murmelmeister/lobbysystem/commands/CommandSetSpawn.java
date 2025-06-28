45266402e32bf3cfda0846f0fd234a086153a0bc
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-28 23:03:01 +0200

    refactor CommandSetSpawn to improve permission handling and message clarity

--- src/main/java/de/murmelmeister/lobbysystem/commands/CommandSetSpawn.java
+++ src/main/java/de/murmelmeister/lobbysystem/commands/CommandSetSpawn.java
@@ -1,39 +1,45 @@
 package de.murmelmeister.lobbysystem.commands;
 
-import de.murmelmeister.lobbysystem.utils.HexColor;
+import de.murmelmeister.lobbysystem.LobbySystem;
+import de.murmelmeister.lobbysystem.api.Locations;
+import de.murmelmeister.lobbysystem.config.MessageConfig;
+import de.murmelmeister.lobbysystem.utils.Messages;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
-public class CommandSetSpawn extends Commands {
+public final class CommandSetSpawn extends Commands {
+    public CommandSetSpawn(LobbySystem plugin) {
+        super(plugin);
+    }
+
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-
-        if (!(sender.hasPermission(Objects.requireNonNull(this.messageConfig.getConfig().getString("Permission.SetSpawn"))))) {
-            setSendMessage(sender, HexColor.format(this.messageConfig.getConfig().getString("Message.NoPermission")));
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        MessageConfig messageConfig = getMessageConfig();
+        Locations locations = getLocations();
+        if (!sender.hasPermission(messageConfig.getMessage(Messages.PERMISSION_SPAWN_SET))) {
+            sendMessage(sender, messageConfig.getMessage(Messages.MESSAGE_NO_PERMISSION));
             return true;
         }
 
         Player player = sender instanceof Player ? (Player) sender : null;
 
         if (player == null) {
-            setSendMessage(sender, HexColor.format(this.messageConfig.getConfig().getString("Message.NoConsole")));
+            sendMessage(sender, messageConfig.getMessage(Messages.MESSAGE_NO_CONSOLE));
             return true;
         }
 
-        this.locationUtil.setLocation("Spawn", player.getLocation());
-        setSendMessage(player, HexColor.format(this.messageConfig.getConfig().getString("Message.SetSpawn")));
-
+        locations.setLocation("Spawn", player.getLocation());
+        sendMessage(player, messageConfig.getMessage(Messages.MESSAGE_SPAWN_SET));
         return true;
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        return null;
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String @NotNull [] args) {
+        return Collections.emptyList();
     }
 }
